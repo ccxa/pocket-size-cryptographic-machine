a0866904a3178841171f11d660f8037a97937740
Janel <janel.me@yahoo.com>
2018-03-15 01:05:08 +0330

    Colorful print functions transfred to ui.py

--- Encoder.py
+++ Encoder.py
@@ -1,20 +1,6 @@
 import hashlib
 import os
-
-def prLightPurple(text):
-    print("\033[36m{}\033[00m".format(text))
-def prLightPurple2(text):
-    print("\033[36m{}\033[00m".format(text)),
-def prBlue(text):
-    print("\033[34m{}\033[00m".format(text))
-def prBlue2(text):
-    print("\033[34m{}\033[00m".format(text)),
-def prRed(text):
-    print("\033[91m{}\033[00m" .format(text))
-def prRed2(text):
-    print("\033[91m{}\033[00m" .format(text)),
-def prGreen(text):
-    print("\033[32m{}\033[00m" .format(text))
+from ui import *
 
 def enc(cargo,kind):
     #--------------------------------------------------------------------------- [Alphabet]
---
+++ ui.py
@@ -0,0 +1,14 @@
+def prLightPurple(text):
+    print("\033[36m{}\033[00m".format(text))
+def prLightPurple2(text):
+    print("\033[36m{}\033[00m".format(text)),
+def prBlue(text):
+    print("\033[34m{}\033[00m".format(text))
+def prBlue2(text):
+    print("\033[34m{}\033[00m".format(text)),
+def prRed(text):
+    print("\033[91m{}\033[00m" .format(text))
+def prRed2(text):
+    print("\033[91m{}\033[00m" .format(text)),
+def prGreen(text):
+    print("\033[32m{}\033[00m" .format(text))
