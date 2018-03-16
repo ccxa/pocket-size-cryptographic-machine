b59d631062272be4145189a395f6e5f1fcb5a553
Janel <janel.me@yahoo.com>
2018-03-16 03:35:03 +0330

    ui.py added spaces between functions

--- ui.py
+++ ui.py
@@ -1,14 +1,27 @@
 def print_purple1(text):
     print("\033[36m{}\033[00m".format(text))
+
+
 def print_purple2(text):
     print("\033[36m{}\033[00m".format(text)),
+
+
 def print_blue(text):
     print("\033[34m{}\033[00m".format(text))
+
+
 def print_blue2(text):
     print("\033[34m{}\033[00m".format(text)),
+
+
 def print_red(text):
     print("\033[91m{}\033[00m" .format(text))
+
+
 def print_red2(text):
     print("\033[91m{}\033[00m" .format(text)),
+
+
 def print_green(text):
     print("\033[32m{}\033[00m" .format(text))
+
