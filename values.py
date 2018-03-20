31d06da2d06408c6d5d8e2c35f3b93be6d92d148
Janel <janel.me@yahoo.com>
2018-03-21 00:00:00 +0330

    help message added to values.py

--- values.py
+++ values.py
@@ -6,3 +6,14 @@ enc_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
 about = '''Its a simple program to Encrypt & Decrypt Texts & Text Files
 you can use it to encrypt secret messages. also you can encrypt all
 your passwords with this program and then save them where you want.'''
+
+help_message = """Args guide:
+[-e] means encrypting sth
+[-d] means decrypting sth
+[-t] text to encrypt or decrypt
+[-f] file to encrypt or decrypt
+
+Examples for encrypting text or text-file:
+ 
+$ python3 Encoder.py -e -t
+$ python3 Encoder.py -e -f '<file path to read>'"""
