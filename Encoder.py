ceefe9e72113fb3cebce033234227c07229e9872
Janel <janel.me@yahoo.com>
2018-03-20 13:17:18 +0330

    Just cleaned

--- Encoder.py
+++ Encoder.py
@@ -4,67 +4,66 @@ from ui import *
 import graphy
 
 
-
 while True:
     os.system('clear')
     print_colorful('Menu', 'blue')
-    print_colorful('---------------------------------------------------------------------', 'purple')
+    print_colorful('-' * 69, 'purple')
     chose = input('[1]Encrypt  [2]Decrypt  [3]Help & About  [4]Exit\n>>  ')
 
-    if chose in ['ENCRYPT','encrypt','Encrypt','1']:
+    if chose in ['ENCRYPT', 'encrypt', 'Encrypt', '1']:
         while True:
             os.system('clear')
             print_colorful('Menu > Encrypt', 'blue')
-            print_colorful('---------------------------------------------------------------------', 'purple')
+            print_colorful('-' * 69, 'purple')
             print('[1]Import File  [2]Input Text  [3]Cancel')
             chose = input('>> ')
 
-            if   chose == '1':
+            if chose == '1':
                 os.system('clear')
                 print_colorful('Menu > Encrypt > Import File', 'blue')
-                print_colorful('---------------------------------------------------------------------', 'purple')
+                print_colorful('-' * 69, 'purple')
                 filename = input('Import File name, or leave it empty to Cancel.\n>> ')
                 if filename == '':
                     break
                 else:
-                    graphy.enc(filename,'file')
+                    graphy.enc(filename, 'file')
                     break
             elif chose == '2':
                 os.system('clear')
                 print_colorful('Menu > Encrypt > Import Text', 'blue')
-                print_colorful('---------------------------------------------------------------------', 'purple')
+                print_colorful('-' * 69, 'purple')
                 text = input('Import Text, or leave it empty to Cancel.\n>> ')
                 if text == '':
                     os.system('clear')
                     break
                 else:
-                    graphy.enc(text,'text')
+                    graphy.enc(text, 'text')
                     break
             elif chose == '3':
                 break
             else:
                 print('Invalid!')
-    elif chose in ['DECRYPT','decrypt','Decrypt','2'] :
+    elif chose in ['DECRYPT', 'decrypt', 'Decrypt', '2']:
         while True:
             os.system('clear')
             print_colorful('Menu > Decrypt', 'blue')
-            print_colorful('---------------------------------------------------------------------', 'purple')
+            print_colorful('-' * 69, 'purple')
             print('[1]Import File  [2]Input Text  [3]Cancel')
             chose = input('>> ')
             if chose == '1':
                 os.system('clear')
                 print_colorful('Menu > Decrypt > Import File', 'blue')
-                print_colorful('---------------------------------------------------------------------', 'purple')
+                print_colorful('-' * 69, 'purple')
                 filename = input('Import File name, or leave it empty to Cancel.\n>> ')
                 if filename == '':
                     break
                 else:
-                    graphy.dec(filename,'file')
+                    graphy.dec(filename, 'file')
                     break
             elif chose == '2':
                 os.system('clear')
                 print_colorful('Menu > Decrypt > Import Text', 'blue')
-                print_colorful('---------------------------------------------------------------------', 'purple')
+                print_colorful('-' * 69, 'purple')
                 text = input('Import Text, or leave it empty to Cancel.\n>> ')
                 if text == '':
                     break
@@ -78,7 +77,7 @@ while True:
     elif chose in ['HELP', 'help', 'Help', '3']:
         os.system('clear')
         print_colorful('Menu > Help & About', 'blue')
-        print_colorful('---------------------------------------------------------------------', 'purple')
+        print_colorful('-' * 69, 'purple')
         print(values.about)
         print('Trust it. powered by SHA256!'),
         print_colorful('created by My own', 'red2')
@@ -86,8 +85,8 @@ while True:
         print_colorful('>>', 'red2')
         print_colorful('Github: Github.com/ccxa', 'blue')
         print_colorful('>>', 'red2')
-        print_colorful('---------------------------------------------------------------------', 'purple')
-        note=input('')
+        print_colorful('-' * 69, 'purple')
+        note = input('')
     elif chose == '4':
         os.system('clear')
         exit()
