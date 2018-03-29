f87d7561c5f60f907e39cdf85cb2cfd1cce38707
Janel <janel.me@yahoo.com>
2018-03-29 22:07:13 +0430

    reformed and cleaned

--- Encoder.py
+++ Encoder.py
@@ -7,25 +7,21 @@ args = sys.argv
 if args[1] in ['-h', '--help']:
     print(values.help_message)
 
-elif args[1] == "-e":
+elif args[1] == "-e" and args[2] == "-t":
+    _text = input("Enter text to encrypt:\n>> ")
+    secret = input("Enter a secret to encrypt string:\n>> ")
+    graphy.enc(_text, secret, 'text')
 
-    if args[2] == "-t":
-        _text = input("Enter text to encrypt:\n>> ")
-        secret = input("Enter a secret to encrypt string:\n>> ")
-        graphy.enc(_text, secret, 'text')
+elif args[1] == "-e" and args[2] == "-f":
+    file_path = args[3]
+    secret = input("Enter a secret to encrypt string:\n>> ")
+    graphy.enc(file_path, secret, "file")
 
-    elif args[2] == "-f":
-        file_path = args[3]
-        secret = input("Enter a secret to encrypt string:\n>> ")
-        graphy.enc(file_path, secret, "file")
+elif args[1] == "-d" and args[2] == "-t":
+    _text = input("Enter encrypted text to decrypt:\n>> ")
+    secret = input("Enter secret to decrypt the data:\n>> ")
+    graphy.dec(_text, secret, 'text')
 
-elif args[1] == "-d":
-
-    if args[2] == "-t":
-        _text = input("Enter encrypted text to decrypt:\n>> ")
-        secret = input("Enter secret to decrypt the data:\n>> ")
-        graphy.dec(_text, secret, 'text')
-
-    elif args[2] == "-f":
-        file_path = args[3]
-        graphy.dec(file_path, "file")
+elif args[1] == "-d" and args[2] == "-f":
+    file_path = args[3]
+    graphy.dec(file_path, "file")
