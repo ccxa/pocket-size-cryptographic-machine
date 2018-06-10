2692d23e103041bb39ef7ff5462ed2fa040fc91d
Janel <janel.me@yahoo.com>
2018-06-10 15:00:00 +0430

    red2 color removed

--- ui.py
+++ ui.py
@@ -1,8 +1,6 @@
 def print_colorful(text, color):
     if color == "green":
         print("\033[32m{}\033[00m".format(text))
-    elif color == "red2":
-        print("\033[91m{}\033[00m".format(text))
     elif color == "red":
         print("\033[91m{}\033[00m".format(text))
     elif color == "blue2":
