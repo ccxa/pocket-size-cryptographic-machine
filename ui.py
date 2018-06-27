f82d637794332d350a935a96bdd9baf864fa3f2b
Janel <janel.me@yahoo.com>
2018-06-28 00:00:00 +0430

    colors fixed ui.py

--- ui.py
+++ ui.py
@@ -1,13 +1,10 @@
 def print_colorful(text, color):
     if color == "green":
+        print("\033[34m{}\033[00m".format(text))
+    elif color == "dark_green":
         print("\033[32m{}\033[00m".format(text))
     elif color == "red":
         print("\033[91m{}\033[00m".format(text))
-    elif color == "blue2":
-        print("\033[34m{}\033[00m".format(text))
     elif color == "blue":
-        print("\033[34m{}\033[00m".format(text))
-    elif color == "purple2":
         print("\033[36m{}\033[00m".format(text))
-    elif color == "purple":
-        print("\033[36m{}\033[00m".format(text))
+
