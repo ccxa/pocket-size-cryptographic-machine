ec9b21c1dc4129b4ec7f24a00a6f5194e67f7cbe
Janel <janel.me@yahoo.com>
2019-04-19 00:00:00 +0430

    removed extra commas

--- ui.py
+++ ui.py
@@ -2,14 +2,14 @@ def print_colorful(text, color):
     if color == "green":
         print("\033[32m{}\033[00m".format(text))
     elif color == "red2":
-        print("\033[91m{}\033[00m".format(text)),
+        print("\033[91m{}\033[00m".format(text))
     elif color == "red":
         print("\033[91m{}\033[00m".format(text))
     elif color == "blue2":
-        print("\033[34m{}\033[00m".format(text)),
+        print("\033[34m{}\033[00m".format(text))
     elif color == "blue":
         print("\033[34m{}\033[00m".format(text))
     elif color == "purple2":
-        print("\033[36m{}\033[00m".format(text)),
+        print("\033[36m{}\033[00m".format(text))
     elif color == "purple":
         print("\033[36m{}\033[00m".format(text))
